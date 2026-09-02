93a4b1287262a3bd363ae72b81b1d0ff83b1c251
matheusmaziero2005-tech <matheus.maziero2005@gmail.com>
2026-09-02 09:09:28 -0300

    Update MainMemoria.java

--- app/MainMemoria.java
+++ app/MainMemoria.java
@@ -1,1 +1,20 @@
+package app;
 
+import model.Usuario;
+
+public class MainMemoria {
+    public static void main(String[] args) {
+        Usuario u1 = new Usuario("Ana", "ana@dominio.com", true);
+        Usuario u2 = new Usuario("Ana Maria", "ana@dominio.com", false);
+
+        if (u1.equals(u2)) {
+            System.out.println("Resultado: Os usuários são logicamente equivalentes (mesmo e-mail).");
+        } else {
+            System.out.println("Resultado: Usuários diferentes.");
+        }
+
+        // Acesso aos membros estáticos diretamente pela Classe
+        System.out.println("Plataforma: " + Usuario.NOME_PLATAFORMA);
+        System.out.println("Total de Usuários cadastrados: " + Usuario.getTotalUsuarios());
+    }
+}
